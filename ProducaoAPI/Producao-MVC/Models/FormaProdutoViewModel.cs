d0f3d79fe5f1af558d015c2597fe1d4d0643f413
Nicole Sypriany <nicolesypriany@gmail.com>
2025-03-16 23:24:40 -0300

    start forma data validations

--- ProducaoAPI/Producao-MVC/Models/FormaProdutoViewModel.cs
+++ ProducaoAPI/Producao-MVC/Models/FormaProdutoViewModel.cs
@@ -6,11 +6,15 @@ namespace Producao_MVC.Models
     public class FormaProdutoViewModel
     {
         public FormaResponse Forma { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O produto é obrigatório.")]
         public int ProdutoId { get; set; }
+        [Range(1, 1000, ErrorMessage = "O número de peças por ciclo deve ser maior do que 0.")]
         public int PecasPorCiclo { get; set; }
         public IEnumerable<ProdutoResponse> Produtos { get; set; }
         public List<MaquinaResponse> Maquinas { get; set; }
+        [Required(ErrorMessage = "A máquina é obrigatória.")]
         public List<MaquinaCheckboxViewModel> MaquinasCheckbox { get; set; }
     }
 }
